773f2ca7b63ab3bdf0cb7428fbba8951b5c79df4
LazzaFisio <lazzarinleonardo00@gmail.com>
2022-05-12 11:58:15 +0200

    Cambiamento del messaggio nella connessione al DB

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ let express = require('express');
 let app = express();
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
+//Configura le env variables
 dotenv.config();
 
 //Connessione a MongoDB
 mongoose.connect(
     `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@freecluster.xj48j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     function (error) {
-        console.log("Connected to Trading database")
+        console.log(error != null ? `DB connection error: ${error.message}` : 'Connected to the MongoDB');
     });
 
 //ROUTING
 const index = require("./routes/index.js");
 
 app.use(express.json()); //Used to parse JSON bodies
-app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //FRONTEND
 app.use("/", express.static("www"));
