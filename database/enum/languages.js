43d30f8f2dfe00f632c30fa4f737821d8c274988
LazzaFisio <lazzarinleonardo00@gmail.com>
2022-05-26 16:30:20 +0200

    Aggiunta metodo getWordsFields
    In base alla lingua passato il metodo ritorna i campi da verificare nello schema word

--- database/enum/languages.js
+++ database/enum/languages.js
@@ -1,4 +1,17 @@
 module.exports = class Languages {
     static IT = 0
     static EN = 1
+
+    /**
+     * Ottiene i campi in base al tipo
+     * @param {Number} type Tipo di linguaggio
+     * @return {String[]} pos 0: chiave per il nome. pos 1: chiave per la lunghezza
+     */
+    static getWordFields(type) {
+        switch (type) {
+            case Languages.IT: return ['it', 'it_length'];
+            case Languages.EN: return ['en', 'en_length'];
+            default: throw new Error("Language type not supported");
+        }
+    }
 }
