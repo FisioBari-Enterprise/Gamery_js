3e50dc15829774593fdf9700d29891ebf1357abb
LazzaFisio <lazzarinleonardo00@gmail.com>
2022-05-26 16:49:52 +0200

    Spostamento funzione di generazione nuovo round
    Spostamento dal file word in classes a singleGame in classes

--- classes/game/words.js
+++ classes/game/words.js
@@ -5,9 +5,7 @@ const GameRound = require("../../database/game/gameRound");
 const { MaxWordForRound } = require('../../config');
 
 module.exports = {
-    saveWords,
-    generateNewRound,
-    timeForRound
+    saveWords
 }
 
 /**
@@ -55,72 +53,4 @@ async function saveWords(res, words) {
         i += 1;
     }
     StaticFunctions.sendSuccess(res, true);
-}
-
-/**
- * Genera un nuovo round in base alle parole che sono già state usate
- * @param { * } game Modello del gioco di riferimento
- * @return {Promise<void>}
- */
-async function generateNewRound(game) {
-    // Controllo conclusione della partita
-    if (game.complete ) {
-        throw "Game already completed";
-    }
-    // Controllo del non completamento dell'ultimo round
-    const rounds = await GameRound.find({game: new ObjectId(game._id), word_insert: { $eq: null }, round: game.max_round}).exec();
-    if (rounds.length > 0) {
-        throw "You haven't completed last round"
-    }
-    
-    //Ottengo le n parole da inserire all'interno del round
-    const nWord = Math.trunc(((game.max_round / 3) + 1) * 4)
-    const maxLength = Math.trunc(nWord / 4) * 6;
-    let words = await Words.aggregate([
-        {
-            $match: {
-                en_length: { $lte: maxLength }
-            }
-        },
-        { $sample: { size: nWord }}
-    ]).exec();
-
-    //Creazione round
-    let round = new GameRound({
-        game: game._id,
-        round: game.max_round + 1,
-        words: []
-    })
-    for(const word of words){
-        round.words.push({
-            word: word._id
-        })
-    } 
-
-    await round.save();
-
-    // Aggiorna il campo round della partita
-    game.max_round += 1;
-    game.memorize_time_for_round = timeForRound(words, false);
-    game.writing_time_for_round = timeForRound(words);
-    await game.save();
-}
-
-/**
- * Ottiene il numero di secondi assegnati per inserire o ricordare le parole
- * @param {[*]} words Lista delle parole
- * @param {boolean} insert Indica se calcolare il tempo per l'inserimento o per la memorizzazione
- * @returns {number} Tempo calcolato
- */
-function timeForRound(words, insert=true) {
-    let tot = 0;
-    for (const word of words) {
-        if (word.en_length == null) {
-            throw "Length not found for word " + word.en;
-        }
-        const base = insert ? 4 : 3;
-        const molt = Math.trunc(word.en_length / 5) + 1;
-        tot += base * molt;
-    }
-    return tot;
 }
