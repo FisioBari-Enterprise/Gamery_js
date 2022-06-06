a7264176e3cfdcfab86976cd6f9486564e747dde
jacopo <jacopo.prescia@gmail.com>
2022-06-06 16:57:56 +0200

    Update frontend settings

--- frontend/src/app/dialogs/settings/settings.component.ts
+++ frontend/src/app/dialogs/settings/settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DialogManagerService} from "../../services/dialog-manager.service";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-settings',
@@ -7,16 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<SettingsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit(): void {
   }
 
   /*Funzione che chiude il dialog quando viene cliccato il bottone di chiusura in alto a dx*/
   close() {
-
+    this.dialogRef.close();
   }
 
-  
+
 
 }
